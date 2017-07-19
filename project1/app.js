0c72ef27916e458b2ea8d798bf13f0481c29c6ad
aaronsato <2asato@gmail.com>
2017-07-19 10:41:43 -0700

    array of emojis

--- project1/app.js
+++ project1/app.js
@@ -3,6 +3,24 @@ $(() => {
 const $test = String.fromCodePoint( 0x1F600 );
 $('#content').text($test);
 
+const emojis = [
+  {
+    name: 'Grinning Face',
+    codepoint: 0x1F600,
+  },
+  {
+    name: 'Crying Face',
+    codepoint: 0x1f622,
+  },
+  {
+    name: 'Winking Face',
+    codepoint: 0x1F609,
+  },
+  {
+    name: 'Smirking Face'
+    codepoint: 0x1F60F,
+  }
+]
 
 
 
