6a867df87f23c62c840b3e772823b6bb98309cb2
aaronsato <2asato@gmail.com>
2017-07-21 13:30:19 -0700

    shuffle emojis array

--- project1/app.js
+++ project1/app.js
@@ -1,6 +1,7 @@
 $(() => {
 
   let score = 0;
+  let $emojisMoved = [];
 
 // array of emojis, codepoints are used to display the emoji
 const $emojis = [
@@ -25,56 +26,80 @@ const $emojis = [
     class: 'smirking',
   }
 ];
+// console.log($emojis);
 
+// fisher yates shuffle to get random order of $emojis
+let x = $emojis.length, y, temp;
 
-const usedAnswers = [];
+while(--x > 0){
+  y = Math.floor(Math.random()*(x+1));
+  temp = $emojis[y];
+  $emojis[y] = $emojis[x];
+  $emojis[x] = temp;
+}
+console.log($emojis);
+console.log($emojis[1]);
+
+for (let i = 0; i < $emojis.length; i++ ){
+  $emojisMoved.push($emojis[i]);
+}
+console.log($emojisMoved);
+
 
 // pulls random emoji from array
-const $randomEmoji = $emojis[Math.floor(Math.random()*4)];
+const $randomEmojiImage = $emojis[Math.floor(Math.random()*4)];
+// const $randomEmoji1 = Math.floor(Math.random()*($emojisMoved.length-1));
+// $emojisMoved.splice($randomEmoji1, 1)
+// const $randomEmoji2 = Math.floor(Math.random()*($emojisMoved.length-1));
+// $emojisMoved.splice($randomEmoji2, 1)
+// const $randomEmoji3 = Math.floor(Math.random()*($emojisMoved.length-1));
+// $emojisMoved.splice($randomEmoji3, 1)
+// const $randomEmoji4 = Math.floor(Math.random()*($emojisMoved.length-1));
+// $emojisMoved.splice($randomEmoji4, 1)
+
+// console.log($randomEmoji1);
+// console.log($randomEmoji2);
+// console.log($randomEmoji3);
+// console.log($randomEmoji4);
+// console.log($emojisMoved);
+
   // console.log($randomEmoji);
   // console.log($randomEmoji.name);
   // console.log($randomEmoji.codepoint);
 
 // display random emoji on DOM
 const $displayEmoji = () => {
-  const $emojiCodepoint = String.fromCodePoint($randomEmoji.codepoint);
+  const $emojiCodepoint = String.fromCodePoint($randomEmojiImage.codepoint);
     // console.log($emojiCodepoint);
     $('#content').text($emojiCodepoint);
 }
 $displayEmoji();
 
-// let $headerMsg = String.fromCodePoint(0x1F1EA);
-//   $headerMsg += String.fromCodePoint(0x1F1F2);
-//   $headerMsg += String.fromCodePoint(0x1F1F4);
-//   $headerMsg += String.fromCodePoint(0x1F1EF);
-//   $headerMsg += String.fromCodePoint(0x1F1EE);
-//   $headerMsg += String.fromCodePoint(0x1F1F8);
-//
-//   $('h1').text($headerMsg);
-
 
 // display emoji names in answer buttons
 const $displayNames = () => {
-  // for (let i = 0; i < $emojis.length; i++) {
+  for (let i = 0; i < $emojis.length; i++) {
   //   console.log($emojis[i]);
-    $('#one').text($emojis[0].name);
-    $('#two').text($emojis[1].name);
-    $('#three').text($emojis[2].name);
-    $('#four').text($emojis[3].name);
+    // $('#one').text($randomEmojiImage.name);
+    // $('#two').text($randomEmojiImage.name);
+    // $('#three').text($randomEmojiImage.name);
+    // $('#four').text($randomEmojiImage.name);
+    $('.answer-button').eq(i).text($emojis[Math.floor(Math.random()*4)].name);
+}
 }
 $displayNames();
 
 const checkClick = () => {
   check = $("button").click(function() {
-      console.log((this.id)); // or alert($(this).attr('id'));
+      console.log((this.id));
   });
 }
 checkClick();
 
 const rightOrWrong = () => {
   for (let i = 0; i < $emojis.length; i++) {
-    if ($('.answer-button').text($emojis[i].name === $randomEmoji.name)) {
-      console.log('RIGHT!');
+    if ($('.answer-button').text($emojis[i].name === $randomEmojiImage.name)) {
+      console.log('RIGHT');
       score++;
       updateScore();
     } else {
@@ -87,6 +112,14 @@ const updateScore = () => {
      $('#score').text('SCORE: ' + score);
   }
   updateScore();
+
+
+
+nextRound = () => {
+  $displayEmoji()
+  $displayNames()
+}
+
 
 
 
